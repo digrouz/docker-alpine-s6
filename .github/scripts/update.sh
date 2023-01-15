eaa39a18d9c0d645f16884eaebe96096e1cc09a0
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2023-01-15 10:40:29 +0000

    auto-update workflow

---
+++ .github/scripts/update.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+DDCLIENT_URL="https://api.github.com/repos/just-containers/s6-overlay/releases"
+
+FULL_LAST_VERSION=$(curl -SsL ${DDCLIENT_URL} | \
+              jq -r -c '.[] | select( .prerelease == false ) | .tag_name' |\
+              head -1 \
+              )
+LAST_VERSION="${FULL_LAST_VERSION:1}"
+
+sed -i -e "s|DDCLIENT_VERSION='.*'|DDCLIENT_VERSION='${LAST_VERSION}'|" Dockerfile*
+
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+  # Working directory clean
+  echo "No new version available!"
+else 
+  # Uncommitted changes
+  git commit -a -m "update to version: ${LAST_VERSION}"
+  git push
+fi
