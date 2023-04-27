c700be81356dc3becc985419820d89f12aa59ae5
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2023-04-27 06:36:49 +0000

    fix empty version

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -8,7 +8,9 @@ FULL_LAST_VERSION=$(curl -SsL ${S6_URL} | \
               )
 LAST_VERSION="${FULL_LAST_VERSION:1}"
 
-sed -i -e "s|S6_VERSION='.*'|S6_VERSION='${LAST_VERSION}'|" Dockerfile*
+if [ "${LAST_VERSION}" ]; then
+  sed -i -e "s|S6_VERSION='.*'|S6_VERSION='${LAST_VERSION}'|" Dockerfile*
+fi
 
 if output=$(git status --porcelain) && [ -z "$output" ]; then
   # Working directory clean
